46d400dd6c271bc13df516b37a3465a7ff34cbf3
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-07-14 11:33:32 +0530

    🙂

--- RiZoeL.py
+++ RiZoeL.py
@@ -753,7 +753,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"♡︎ʀɪᴢᴏᴇʟ sᴘᴀᴍʙᴏᴛ♡︎\n█▀█ █▀█ █▄░█ █▀▀\n█▀▀ █▄█ █░▀█ █▄█\n`{ms}` ᴍs")
+        await event.edit(f"█▀█ █▀█ █▄░█ █▀▀\n█▀▀ █▄█ █░▀█ █▄█\n♡︎ʀɪᴢᴏᴇʟ sᴘᴀᴍʙᴏᴛ♡︎\n`{ms}` ᴍs")
 
 
 
